899eff799f2a0e0730f14ee908a7dd931d57bf68
Harpreet Singh <harpreetsingh14388@live.com>
2017-04-27 01:25:52 +0530

    SignalR added

---
+++ Connect/Echo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Connect {
+    public class Echo : Hub {
+        public void Hello() {
+            Clients.All.hello();
+        }
+    }
+}
